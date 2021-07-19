690446c13e0ddf94f5b83688a84e9e815ca5db51
jiqiang <johnson.ji@zenlayer.com>
2021-07-19 10:43:24 +0800

    修复bug

--- utils.go
+++ utils.go
@@ -1,21 +1,33 @@
 package commontools
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 	"os/exec"
 )
 
-func LinuxActioncommand(input *os.File) (output *os.File) {
+// 返回命令到终端
+func LinuxActioncommand(input *os.File) bytes.Buffer {
 	cmd := exec.Command("sh")
 	cmd.Stdin = input
-	cmd.Stdout = output
+
+	var ret bytes.Buffer
+	cmd.Stdout = &ret
 	cmd.Run()
-	return output
+	return ret
 
 }
 
 // 获取
-func hostname() string {
+func Hostname() string {
 	hostname, _ := os.Hostname()
 	return hostname
+}
+
+func main() {
+	input := os.Stdin
+	var output bytes.Buffer
+	output = LinuxActioncommand(input)
+	fmt.Println(output.String())
 }
